4c6c13f8cb2f2f3c7022c7e9c1bb83f88e1abb27
monicaiyb <monicaiyb@gmail.com>
2022-05-20 17:10:37 +0300

    Add date time

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApplication1
     {
         private string _firstName;
         private string _lastName;
+        private double d1;
+        private double d2;
 
+ 
         public string FirstName
         {
             get
@@ -36,7 +39,30 @@ namespace ConsoleApplication1
                 _lastName = value;
             }
         }
+        public double Number1
+        {
+            get
+            {
+                return d1;
+            }
 
+            set
+            {
+                d1 = value;
+            }
+        }
+        public double Number2
+        {
+            get
+            {
+                return d2;
+            }
+
+            set
+            {
+                d2 = value;
+            }
+        }
         public abstract void FullName();
     }
     // Inheriting class from the base that uses the methods and variables from the abstract class
@@ -48,28 +74,43 @@ namespace ConsoleApplication1
             Program p = new Program();
             p.FirstName = "Monica";
             p.LastName = "Iyabo";
-            p.FullName();
+            p.Number1 = 12345678912345.5d;
+            p.Number2 = 1.123456789123456d;
+            p.FullName(); 
+
+
             Console.ReadLine();
 
-        }
+                   }
         public override void FullName()
         {
-            Console.WriteLine("Full Name:" + FirstName + " " + LastName);
-        }
-    }
+            Console.WriteLine("Full Name:" + FirstName + " " + LastName+" "+Number1+" "+Number2);
+            DateTime dt2 = new DateTime(2015, 12, 31);
+            DateTime currentDateTime = DateTime.Now;
+            DateTime minDateTimeValue = DateTime.MinValue;
+            DateTime todaysDate = DateTime.Today; // returns today's date
 
-    public sealed class Employee
-    {
-        string firstName;
-        string lastName;
-    }
-    class Program1 : Employee
-    {
-        static void Main(string[] args)
-        {
+            TimeSpan time = new TimeSpan(10, 5, 25, 50);
+                
+            Console.WriteLine(dt2+" "+ currentDateTime+"   "+ todaysDate+"   "+ minDateTimeValue+ "    "+ time);
 
+
+            var str = "5/122020";
+            DateTime dt;
+
+            var isValidDate = DateTime.TryParse(str, out dt);
+
+            if (isValidDate)
+                Console.WriteLine(dt);
+            else
+                Console.WriteLine($"{str} is not a valid date string");
         }
+
     }
+  
+    
+
+   
 
 
 }
