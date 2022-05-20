30a4bdc0d575f2637efa42e8de9977df3f553135
monicaiyb <monicaiyb@gmail.com>
2022-05-20 14:35:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+// C# program to illustrate the
+// concept of inheritance
+using System;
+namespace ConsoleApplication1
+{
+    //An abstract does do anything within itself
+    public abstract class Customer
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public abstract void FullName();
+    }
+    // Inheriting class from the base that uses the methods and variables from the abstract class
+    class Program: Customer
+    {
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.FirstName = "Monica";
+            p.LastName = "Iyabo";
+            p.FullName();
+            Console.ReadLine();
+
+        }
+        public override void FullName()
+        {
+            Console.WriteLine("Full Name:" + FirstName + " " + LastName);
+        }
+    }
+
+    public sealed class Employee
+    {
+        string firstName;
+        string lastName;
+    }
+    class Program1 : Employee
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+}
+
